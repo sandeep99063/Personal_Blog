fb78569b509b626db330c8b226530e69e5053f08
Sandeep Kumar Singh <sandeep99063@gmail.com>
2015-07-17 23:56:49 +0530

    All Done Except Content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require masonry.pkgd.min.js
 //= require turbolinks
 
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'project'=> 'pages#project'
   get 'present'=> 'pages#present'
   root 'pages#home'
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
